7cfa17ca8f208c85a4145016aa6013ea728ecab9
ninatchke <natchkebia.ni@gmail.com>
2025-07-18 14:35:48 +0200

    ოკ

--- cypress/e2e/account.cy.js
+++ cypress/e2e/account.cy.js
@@ -3,39 +3,43 @@
 //Test 3: Add or edit address
 //Test 4: Change password
 describe('AutomationTestStore User Account Tests', () => {
-  const email = 'testuser123@email.com';
-  const password = 'Password123!';
+  const loginName = 'ninatchke';
+  const password = 'Testing2025!';
 
   beforeEach(() => {
-    cy.login(email, password);
+    cy.login(loginName, password);
   });
 
-  it('should edit account details successfully', () => {
-    cy.contains('Edit Account').click();
-    cy.get('input[name="firstname"]').clear().type('Nino');
+ it('should edit account details', () => {
+    cy.visit('https://automationteststore.com/index.php?rt=account/account');
+     cy.contains('Edit account details').should('be.visible').click({ force: true });
+     cy.wait(3000);
+    cy.get('input[name="firstname"]').clear().type('Natia');
     cy.get('input[name="lastname"]').clear().type('Cypress');
     cy.get('button[title="Continue"]').click();
-    cy.contains('Your account has been successfully updated').should('be.visible');
+    cy.contains('Success: Your account has been successfully updated.').should('be.visible');
   });
 
   it('should add new address successfully', () => {
-    cy.contains('Address Book').click();
+    cy.contains('Manage Address Book').click();
     cy.contains('New Address').click();
-    cy.get('input[name="firstname"]').type('Nino');
+    cy.get('input[name="firstname"]').type('Natia');
     cy.get('input[name="lastname"]').type('Tester');
-    cy.get('input[name="address_1"]').type('123 Cypress Way');
-    cy.get('input[name="city"]').type('Tbilisi');
-    cy.get('input[name="postcode"]').type('0100');
-    cy.get('select[name="country_id"]').select('Georgia');
-    cy.get('select[name="zone_id"]').select(1);
+    cy.get('input[name="company"]').type('Company Tester');
+    cy.get('input[name="address_1"]').type('35 Cypress');
+    cy.get('input[name="city"]').type('Telavi');
+      cy.get('select[name="Region/State"]').select(randomValue || 'Some State');
+    cy.get('input[name="ZIP/PostCode"]').type('42100');
+    cy.get('select[name="country"]').select('Georgia');
     cy.get('button[title="Continue"]').click();
     cy.contains('Your address has been successfully added').should('be.visible');
   });
 
   it('should change password successfully', () => {
     cy.contains('Change Password').click();
-    cy.get('input[name="password"]').type('Password123!');
-    cy.get('input[name="confirm"]').type('Password123!');
+    cy.get('input[name="Current password"]').type('Testing2025!');
+    cy.get('input[name="New Password"]').type('Password123!');
+    cy.get('input[name="confirm New Password"]').type('Password123!');
     cy.get('button[title="Continue"]').click();
     cy.contains('Your password has been successfully updated').should('be.visible');
   });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,6 @@
 Cypress.Commands.add('login', (email, password) => {
   cy.visit('https://automationteststore.com/index.php?rt=account/login');
-  cy.get('input[name="loginname"]').type(email);
-  cy.get('input[name="password"]').type(password);
+  cy.get('input[name="loginname"]').type('ninatchke');
+  cy.get('input[name="password"]').type('Testing2025!');
   cy.get('button[title="Login"]').click();
 });
